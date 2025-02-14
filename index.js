733bb9444b54fca0cedfe14f4ba723f4fd24bce6
Andrei <greabandrei1@gmail.com>
2025-02-14 14:44:04 +0200

    First UI design implementation

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-//........DOM ELments........
-const createQrBtn = document.getElementById("btnCreate")
-const clearQrBtn = document.getElementById("btnClear")
-
-
-//.........Event listener......
-
-createQrBtn.addEventListener("click", generateQR)
-
 
 function generateQR(){
     const qrcode = new QRCode("qrcode", {
         text: "https://emag.com",
-        width: 150,
-        height: 150,
+        width: 100,
+        height: 100,
         colorDark : "#000",
         colorLight : "#f2f2f2",
         correctLevel : QRCode.CorrectLevel.H
@@ -24,12 +15,3 @@ function generateQR(){
 console.log("Hi")
 
 window.onload = generateQR(); //se asigura ca pagina a fost complet incarcata ca sa execute functia.
-
-
-
-/*
-    Cerinte:
-     - creaza interfata asemanatoare cu https://www.the-qrcode-generator.com/
-     - utilizatorul poate customiza URL, width, height, colors
-     - utilizatorul poate genera si sterge QR-ul
-*/
